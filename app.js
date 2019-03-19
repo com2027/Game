8230b52aaeb9d15252af407b6074092de5f7d08c
Kieran Rigby <kieran@kierigby.com>
2019-03-19 01:50:02 +0000

    Cleaned up ready for master branch

--- app.js
+++ app.js
@@ -1,18 +1,42 @@
+/////////////////////////////////////
+// Bounty Hunter Game Server
+// 2019
+// COM2027 - Group 15
+/////////////////////////////////////
+
+//set port to either the deployment port or 3000 for development
 const PORT = process.env.PORT || 3000;
-var app = require('express')();
+
+//include express, http server and socket.io
+var express = require('express');
+const app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+// INITAL SERVER SETUP
+/////////////////////////////////////////////////////////////////////////
+//On the root route, serve the index page which is just a HTML page containing the logo
 app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
+  res.sendFile(__dirname + '/public/index.html');
 });
 
+//Set express to use static files so we can load the logo
+app.use(express.static('public'));
+
+//Start listening on the port and serving web pages.
+http.listen(PORT, function(){
+  console.log('listening on *:' + PORT);
+});
+
+/////////////////////////////////////////////////////////////////////////
+
+
+
+// SOCKET IO GAME STUFF STARTS HERE
+/////////////////////////////////////////////////////////////////////////
 io.on('connection', function(socket){
   console.log('a user connected');
 
   socket.emit('welcome','welcome to the server');
 });
-
-http.listen(PORT, function(){
-  console.log('listening on *:' + PORT);
-});
+/////////////////////////////////////////////////////////////////////////
