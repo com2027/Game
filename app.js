dd5168d771bd7914fa068c238da28a6bb300638f
Kieran Rigby <kieran@kierigby.com>
2019-03-19 01:03:40 +0000

    Fix web sockets for Heroku

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const PORT = process.env.PORT || 3000;
 const io = require("socket.io");
-io.set('origins', '*:*');
+io.configure(function () {
+  io.set("transports", ["xhr-polling"]);
+  io.set("polling duration", 10);
+});
 const server = io.listen(PORT);
 
 console.log("Listening on port " + PORT);
