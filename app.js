d792eb3c9a51eee4c71e13c7d951450c500fbd45
Kieran Rigby <kieran@kierigby.com>
2019-03-19 00:53:13 +0000

    Added socket.io library and wrote a test example

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-console.log("Hello World")
+const PORT = process.env.PORT || 3000;
+const io = require("socket.io");
+const server = io.listen(PORT);
+
+console.log("Listening on port " + PORT);
+
+server.on("connection", function(socket) {
+  console.log("user connected");
+  socket.emit("welcome", "welcome man");
+});
